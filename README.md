6cf279a6cc94778c1784573a6846f1f718ba7422
Böckler, Kevin <Kevin.Boeckler@mach.de>
2023-11-24 14:46:05 +0100

    Add sample test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advent of Code 23
 
 Run ``` ./gradlew run ``` to execute the main runner.
+
+Run ``` ./gradlew test ``` to run all tests.
---
+++ src/test/kotlin/de.kevinboeckler.aoc23/Day1Test.kt
@@ -0,0 +1,13 @@
+package de.kevinboeckler.aoc23
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import kotlin.test.Test
+
+internal class Day1Test {
+
+    @Test
+    fun testDay1Part1() {
+        assertEquals("hallo welt", day1_part1.solve("test"))
+    }
+
+}
