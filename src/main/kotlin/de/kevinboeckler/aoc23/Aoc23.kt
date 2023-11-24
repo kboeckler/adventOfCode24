ff83ed302d35db5a5f1c6f83696c6b6c9f412435
Böckler, Kevin <Kevin.Boeckler@mach.de>
2023-11-24 14:07:28 +0100

    Kotlin runner for solutions

---
+++ src/main/kotlin/de/kevinboeckler/aoc23/Aoc23.kt
@@ -0,0 +1,20 @@
+package de.kevinboeckler.aoc23
+
+import java.io.File
+
+fun main() {
+    solutions.forEach {
+        val input = File("src/main/resources/%s.txt".format(it.name)).readText()
+        println("%s: %s, %s".format(it.name, it.part1.solve(input), it.part2.solve(input)))
+    }
+}
+
+fun interface Part {
+    fun solve(input: String): Any
+}
+
+data class Day(val name: String, val part1: Part, val part2: Part) {
+}
+
+// List all solutions of all days here
+val solutions = listOf(Day("Day1", day1_part1, day1_part2))
---
+++ src/main/kotlin/de/kevinboeckler/aoc23/Day1.kt
@@ -0,0 +1,9 @@
+package de.kevinboeckler.aoc23
+
+val day1_part1 = Part {
+    "hallo welt"
+}
+
+val day1_part2 = Part {
+    42
+}
