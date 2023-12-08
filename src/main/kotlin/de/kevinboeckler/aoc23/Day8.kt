957fa693d08d2f5cdd1d0564b9f80d68ef0149d5
Böckler, Kevin <Kevin.Boeckler@mach.de>
2023-12-08 14:04:17 +0100

    Day8 Part2
    But Part2 prints out the numbers which must be put into lcm function.
    lcm=least common multiple

--- src/main/kotlin/de/kevinboeckler/aoc23/Day8.kt
+++ src/main/kotlin/de/kevinboeckler/aoc23/Day8.kt
@@ -6,9 +6,28 @@ class Day8 : Day() {
         val instructions = input.lines()[0].map { rlToIndex[it]!! }
         val intersections = "(.+) = \\((.+), (.+)\\)".toRegex().findAll(input).map { it.groupValues }
             .map { Intersection(it[1], listOf(it[2], it[3])) }.groupBy { it.from }.mapValues { it.value[0] }
+        return calculateSteps("AAA", "ZZZ".toRegex(), instructions, intersections)
+    }
+
+    override fun part2(input: String): Any {
+        val rlToIndex = mapOf('L' to 0, 'R' to 1)
+        val instructions = input.lines()[0].map { rlToIndex[it]!! }
+        val intersections = "(.+) = \\((.+), (.+)\\)".toRegex().findAll(input).map { it.groupValues }
+            .map { Intersection(it[1], listOf(it[2], it[3])) }.groupBy { it.from }.mapValues { it.value[0] }
+        return intersections.map { it.value.from }.filter { it.endsWith("A") }
+            .map { calculateSteps(it, "..Z".toRegex(), instructions, intersections) }
+            .joinToString(",") { "$it" }.run { "lcm($this)" }
+    }
+
+    private fun calculateSteps(
+        start: String,
+        endCheck: Regex,
+        instructions: List<Int>,
+        intersections: Map<String, Intersection>
+    ): Int {
         var steps = 0
-        var current = "AAA"
-        while (current != "ZZZ") {
+        var current = start
+        while (!current.matches(endCheck)) {
             val instruction = instructions[steps % instructions.size]
             current = intersections[current]!!.target[instruction]
             steps++
@@ -16,25 +35,6 @@ class Day8 : Day() {
         return steps
     }
 
-    override fun part2(input: String): Any {
-        if (true) {
-            // TODO too slow here
-            return "?"
-        }
-        val rlToIndex = mapOf('L' to 0, 'R' to 1)
-        val instructions = input.lines()[0].map { rlToIndex[it]!! }
-        val intersections = "(.+) = \\((.+), (.+)\\)".toRegex().findAll(input).map { it.groupValues }
-            .map { Intersection(it[1], listOf(it[2], it[3])) }.groupBy { it.from }.mapValues { it.value[0] }
-        var steps = 0
-        val endVertices = intersections.flatMap { it.value.target }.filter { it.endsWith("Z") }
-        var current = intersections.map { it.value.from }.filter { it.endsWith("A") }
-        while (current.any { !endVertices.contains(it) }) {
-            val instruction = instructions[steps % instructions.size]
-            current = current.map { intersections[it]!!.target[instruction] }
-            steps++
-        }
-        return steps
-    }
-
     data class Intersection(val from: String, val target: List<String>)
+
 }
