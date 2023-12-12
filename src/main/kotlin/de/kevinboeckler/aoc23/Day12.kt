e0446f4135caa7982d11404fac97b5487c9cc103
kboeckler <kevinboeckler@gmail.com>
2023-12-12 18:59:13 +0100

    Improve Day12 Part 1

--- src/main/kotlin/de/kevinboeckler/aoc23/Day12.kt
+++ src/main/kotlin/de/kevinboeckler/aoc23/Day12.kt
@@ -13,7 +13,7 @@ class Day12 : Day() {
         return input.lines().asSequence().filterNot { it.isBlank() }.map {
             "([.?#]+)".toRegex().find(it)!!.value to "(\\d+)".toRegex().findAll(it)
                 .map { match -> match.value.toInt() }.toList()
-        }.map { Conditions(it.second, unfold(it.first)) }.sumOf { arrangementsOf(it) }
+        }.map { Conditions(unfold(it.second), unfold(it.first)) }.sumOf { arrangementsOf(it) }
     }
 
     data class Conditions(val groups: List<Int>, val record: String)
@@ -22,6 +22,12 @@ class Day12 : Day() {
         val firstUnknown = condition.record.indexOfFirst { it == '?' }
         if (firstUnknown == -1) {
             return if (isFeasible(condition)) 1 else 0
+        }
+        val firstGroups = "(#+[^#])".toRegex().findAll(condition.record.substring(0, firstUnknown))
+            .map { it.value.filter { c -> c == '#' }.length }.toList()
+        if (firstGroups.filterIndexed { i, num -> i >= condition.groups.size || condition.groups[i] != num }
+                .isNotEmpty()) {
+            return 0
         }
         val replaced1 = Conditions(condition.groups, condition.record.replaceFirst('?', '.'))
         val replaced2 = Conditions(condition.groups, condition.record.replaceFirst('?', '#'))
@@ -34,6 +40,10 @@ class Day12 : Day() {
     }
 
     private fun unfold(value: String): String {
-        return (0..4).map { _ -> value }.joinToString("?")
+        return (0..4).joinToString("?") { _ -> value }
+    }
+
+    private fun unfold(values: List<Int>): List<Int> {
+        return (0..4).flatMap { values }
     }
 }
