da68ed39e6def275c1d37515922c54c646d9dac7
Kev <kevinboeckler@gmail.com>
2023-12-15 21:54:09 +0100

    Day15 Complete

--- src/main/kotlin/de/kevinboeckler/aoc23/Day15.kt
+++ src/main/kotlin/de/kevinboeckler/aoc23/Day15.kt
@@ -6,17 +6,26 @@ class Day15 : Day() {
     }
 
     override fun part2(input: String): Any {
-        TODO("Not yet implemented")
+        val storage = (0..255).map { mutableListOf<Pair<String, Int>>() }
+        input.split(",").forEach {
+            val label = it.substring(0, it.indexOfAny(charArrayOf('-', '=')))
+            val hash = hashOf(label)
+            if (it.contains("-")) {
+                storage[hash].removeAll { item -> item.first == label }
+            } else {
+                if (storage[hash].any { item -> item.first == label }) {
+                    storage[hash][storage[hash].indexOfFirst { item -> item.first == label }] =
+                        label to it.replace("$label=", "").toInt()
+                } else {
+                    storage[hash].add(label to it.replace("$label=", "").toInt())
+                }
+            }
+        }
+        return storage.mapIndexed { i, pairs -> pairs.mapIndexed { j, pair -> (i + 1) * (j + 1) * pair.second }.sum() }
+            .sum()
     }
 
     private fun hashOf(word: String): Int {
-        var value = 0
-        word.forEach {
-            value += it.code
-            value *= 17
-            value %= 256
-
-        }
-        return value
+        return (0.toChar() + word).map { it.code }.reduce { a, b -> ((a + b) * 17) % 256 }
     }
 }
--- src/test/kotlin/de/kevinboeckler/aoc23/Day15Test.kt
+++ src/test/kotlin/de/kevinboeckler/aoc23/Day15Test.kt
@@ -14,4 +14,9 @@ class Day15Test {
     fun part1() {
         assertEquals(1320, Day15().part1("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"))
     }
+
+    @Test
+    fun part2() {
+        assertEquals(145, Day15().part2("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"))
+    }
 }
