77206660480cac2aef3818b47729404a617fa8e6
kboeckler <kevinboeckler@gmail.com>
2024-12-01 09:34:23 +0100

    Day1

--- src/main/kotlin/com/github/kboeckler/aoc24/Day1.kt
+++ src/main/kotlin/com/github/kboeckler/aoc24/Day1.kt
@@ -1,11 +1,45 @@
 package com.github.kboeckler.aoc24
 
+import kotlin.math.abs
+
 class Day1 : Day {
     override fun part1(input: String): Any? {
-        TODO("Not yet implemented")
+        val allIds = input.lines().flatMap(::splitToInteger)
+        val firstIds = allIds.evenItems().sorted()
+        val secondIds = allIds.unevenItems().sorted()
+        return firstIds.mapIndexed { idx, firstId ->
+            firstId - secondIds[idx]
+        }
+            .map(::abs)
+            .sum()
     }
 
     override fun part2(input: String): Any? {
-        TODO("Not yet implemented")
+        val allIds = input.lines().flatMap(::splitToInteger)
+        val firstIds = allIds.evenItems()
+        val secondIds = allIds.unevenItems()
+        return firstIds.map { firstId ->
+            firstId * secondIds.filter { secondId -> firstId == secondId }.count()
+        }
+            .sum()
+    }
+
+    private fun splitToInteger(line: String): Iterable<Int> {
+        val res = Regex("(\\d+)").findAll(line)
+        return res.map { it.groupValues[1].toInt() }.toList()
+    }
+
+    fun <T> List<T>.evenItems(): List<T> {
+        return this.filterIndexed(filterIndexMod2<T>(0))
+    }
+
+    fun <T> List<T>.unevenItems(): List<T> {
+        return this.filterIndexed(filterIndexMod2<T>(1))
+    }
+
+    private fun <T> filterIndexMod2(mod: Int): (index: Int, T) -> Boolean {
+        return { idx, value ->
+            idx % 2 == mod
+        }
     }
 }
--- src/test/kotlin/com/github/kboeckler/aoc24/Day1Test.kt
+++ src/test/kotlin/com/github/kboeckler/aoc24/Day1Test.kt
@@ -1,21 +1,37 @@
 package com.github.kboeckler.aoc24
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class Day1Test {
 
     @Test
     fun part1() {
-        assertThrows<NotImplementedError> {
-            Day1().part1("")
-        }
+        assertEquals(
+            11,
+            Day1().part1(
+                "3   4\n" +
+                        "4   3\n" +
+                        "2   5\n" +
+                        "1   3\n" +
+                        "3   9\n" +
+                        "3   3"
+            )
+        )
     }
 
     @Test
     fun part2() {
-        assertThrows<NotImplementedError> {
-            Day1().part2("")
-        }
+        assertEquals(
+            31,
+            Day1().part2(
+                "3   4\n" +
+                        "4   3\n" +
+                        "2   5\n" +
+                        "1   3\n" +
+                        "3   9\n" +
+                        "3   3"
+            )
+        )
     }
 }
