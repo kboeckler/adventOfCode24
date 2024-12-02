c3f710164b40a1d331458807dd0102711d166b23
Böckler, Kevin <Kevin.Boeckler@mach.de>
2024-12-02 15:07:28 +0100

    Refactor Day2

--- src/main/kotlin/com/github/kboeckler/aoc24/Day2.kt
+++ src/main/kotlin/com/github/kboeckler/aoc24/Day2.kt
@@ -5,19 +5,23 @@ import kotlin.math.abs
 class Day2 : Day {
     override fun part1(input: String): Any {
         return input.lines().map { it.split(" ") }
-            .map { it.map { innerIt -> innerIt.toInt() } }
-            .map { line ->
-                (0..line.size - 2).map { line[it] - line[it + 1] }
-            }
-            .filter { diffs ->
-                (0..diffs.size - 2).all { diffs[it].norm() == diffs[it + 1].norm() }
-            }.filter { diffs ->
-                diffs.all { abs(it) in 1..3 }
-            }.count()
+            .map { it.map { innerIt -> innerIt.toInt() } }.count { indexOfWrongLevel(it) == -1 }
     }
 
     override fun part2(input: String): Any {
         TODO("Not yet implemented")
+    }
+
+    private fun indexOfWrongLevel(levels: List<Int>): Int {
+        val diffs = (0..levels.size - 2).map { levels[it] - levels[it + 1] }
+        val firstLevelNotHavingSameNorm =
+            (0..diffs.size - 2).indexOfFirst { diffs[it].norm() != diffs[it + 1].norm() }
+        val firstLevelNotDifferingInRange = diffs.indexOfFirst { abs(it) !in 1..3 }
+        return if (firstLevelNotHavingSameNorm != -1) {
+            firstLevelNotHavingSameNorm
+        } else if (firstLevelNotDifferingInRange != -1) {
+            firstLevelNotDifferingInRange
+        } else -1
     }
 
     private fun Int.norm(): Int {
