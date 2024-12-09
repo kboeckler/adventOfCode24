ea160805597c0b46b3e83ab43175cba3fc44e049
kboeckler <kevinboeckler@gmail.com>
2024-12-09 07:33:45 +0100

    Day9 just begun

---
+++ src/main/kotlin/com/github/kboeckler/aoc24/Day9.kt
@@ -0,0 +1,21 @@
+package com.github.kboeckler.aoc24
+
+class Day9 : Day {
+    override fun part1(input: String): Any {
+        return input.lines().map { line ->
+            val ids = (0 until line.length / 2 + 1).map { "$it" }.toList()
+            val dots = line.map { "." }.toList()
+            val symbols = ids.zip(dots).flatMap { listOf(it.first, it.second) }
+            val amounts = line.map { "$it".toInt() }
+            val expanded = amounts.zip(symbols).map { assignment ->
+                (0 until assignment.first).map { assignment.second }.joinToString("")
+            }.joinToString("")
+            println(expanded)
+            0
+        }.sum()
+    }
+
+    override fun part2(input: String): Any {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/com/github/kboeckler/aoc24/Day9Test.kt
@@ -0,0 +1,38 @@
+package com.github.kboeckler.aoc24
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day9Test {
+
+    @Test
+    fun part1() {
+        assertEquals(
+            1928,
+            Day9().part1(
+                "2333133121414131402"
+            )
+        )
+    }
+
+    @Test
+    fun part2() {
+        /*assertEquals(
+            34,
+            Day9().part2(
+                "............\n" +
+                        "........0...\n" +
+                        ".....0......\n" +
+                        ".......0....\n" +
+                        "....0.......\n" +
+                        "......A.....\n" +
+                        "............\n" +
+                        "............\n" +
+                        "........A...\n" +
+                        ".........A..\n" +
+                        "............\n" +
+                        "............"
+            )
+        )*/
+    }
+}
