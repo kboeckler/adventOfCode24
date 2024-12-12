4aba99cfd9e0ce44355fcc5683304525ede5f875
kboeckler <kevinboeckler@gmail.com>
2024-12-12 08:18:01 +0100

    Day12 start it

---
+++ src/test/kotlin/com/github/kboeckler/aoc24/Day12Test.kt
@@ -0,0 +1,58 @@
+package com.github.kboeckler.aoc24
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day12Test {
+
+    @Test
+    fun part1_small() {
+        assertEquals(
+            140,
+            Day12().part1(
+                "AAAA\n" +
+                        "BBCD\n" +
+                        "BBCC\n" +
+                        "EEEC"
+            )
+        )
+    }
+
+    @Test
+    fun part1_larger() {
+        assertEquals(
+            1930,
+            Day12().part1(
+                "RRRRIICCFF\n" +
+                        "RRRRIICCCF\n" +
+                        "VVRRRCCFFF\n" +
+                        "VVRCCCJFFF\n" +
+                        "VVVVCJJCFE\n" +
+                        "VVIVCCJJEE\n" +
+                        "VVIIICJJEE\n" +
+                        "MIIIIIJJEE\n" +
+                        "MIIISIJEEE\n" +
+                        "MMMISSJEEE"
+            )
+        )
+    }
+
+    @Test
+    fun part2() {
+        /*assertEquals(
+            2858,
+            Day12().part2(
+                "RRRRIICCFF\n" +
+                        "RRRRIICCCF\n" +
+                        "VVRRRCCFFF\n" +
+                        "VVRCCCJFFF\n" +
+                        "VVVVCJJCFE\n" +
+                        "VVIVCCJJEE\n" +
+                        "VVIIICJJEE\n" +
+                        "MIIIIIJJEE\n" +
+                        "MIIISIJEEE\n" +
+                        "MMMISSJEEE"
+            )
+        )*/
+    }
+}
